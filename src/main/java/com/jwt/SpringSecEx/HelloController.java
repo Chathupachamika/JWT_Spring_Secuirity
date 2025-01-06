65cb24d626a22e663de2b9d44e5e775d9e0d1e19
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-01-06 17:54:30 +0530

    spring secuirity dependency inject

---
+++ src/main/java/com/jwt/SpringSecEx/HelloController.java
@@ -0,0 +1,13 @@
+package com.jwt.SpringSecEx;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome To Log In Page";
+    }
+}
