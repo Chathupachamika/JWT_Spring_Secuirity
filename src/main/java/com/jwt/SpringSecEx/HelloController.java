428f0b3cc4b4829a6d77484d5e71f433851cc3e8
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-01-06 19:00:29 +0530

    spring config

--- src/main/java/com/jwt/SpringSecEx/HelloController.java
+++ src/main/java/com/jwt/SpringSecEx/HelloController.java
@@ -1,5 +1,6 @@
 package com.jwt.SpringSecEx;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String greet(){
-        return "Welcome To Log In Page";
+    public String greet(HttpServletRequest request){
+        return "Welcome To Log In Page" + request.getSession().getId();
     }
 }
